9e1a02acc58788215bdefd000c5b0e7914c16f9c
Andres Arango <andresarangov17@gmail.com>
2017-05-12 15:08:21 -0400

    animal show route, controller

--- js/app.js
+++ js/app.js
@@ -11,10 +11,25 @@ angular
     "$resource",
     LocationFactoryFunction
   ])
+  .factory("AnimalFactory", [
+    "$resource",
+    AnimalFactoryFunction
+  ])
   .controller("LocationIndexController", [
     "LocationFactory",
     LocationIndexControllerFunction
-  ]);
+  ])
+  .controller("LocationShowController", [
+    "LocationFactory",
+    "$stateParams",
+    LocationShowControllerFunction
+  ])
+  .controller("AnimalShowController", [
+    "AnimalFactory",
+    "$stateParams",
+    AnimalShowControllerFunction
+  ])
+
   function RouterFunction($stateProvider){
     $stateProvider
     .state("locationIndex",{
@@ -23,10 +38,36 @@ angular
       controller:"LocationIndexController",
       controllerAs:"vm"
     })
+    .state("animalShow", {
+      url: "/locations/:location_id/animals/:id",
+      templateUrl: "js/ng-views/animal_show.html",
+      controller: "AnimalShowController",
+      controllerAs: "vm"
+    })
+    .state("locationShow", {
+      url: "/locations/:id",
+      templateUrl: "js/ng-views/show.html",
+      controller: "LocationShowController",
+      controllerAs: "vm"
+    })
+
   }
   function LocationFactoryFunction ($resource){
     return $resource("http://localhost:3000/locations/:id",{},{});
   }
+  function AnimalFactoryFunction ($resource){
+    return $resource("http://localhost:3000/locations/:location_id/animals/:id",{},{});
+
+  }
+
   function LocationIndexControllerFunction(LocationFactory){
     this.locations = LocationFactory.query();
+  }
+
+  function LocationShowControllerFunction (LocationFactory, $stateParams) {
+    this.location = LocationFactory.get({id: $stateParams.id})
+  }
+
+  function AnimalShowControllerFunction(AnimalFactory, $stateParams) {
+    this.animal = AnimalFactory.get({location_id: $stateParams.location_id, id: $stateParams.id})
   }
