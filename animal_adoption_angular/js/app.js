abb72fdae591e8074093799c2f92308167c9358a
Luisa Gonzalez <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-05-12 10:43:19 -0400

    created angular module

--- animal_adoption_angular/js/app.js
+++ animal_adoption_angular/js/app.js
@@ -1,3 +1,26 @@
 angular
   .module("animal_farm", [
+    "ui.router",
+    "ngResource"
   ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .factory("LocationFactory", [
+    "$resource",
+    LocationFactoryFunction
+  ])
+  .controller("LocationIndexController") [
+    "LocationFactory",
+    LocationIndexControllerFunction
+  ]);
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("locationIndex",{
+      url:"/locations",
+      templateUrl:"js/ng-views/index.html",
+      controller:"LocationIndexController",
+      controllerAs:"vm"
+    })
+  }
