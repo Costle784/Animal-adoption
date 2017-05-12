668a04d0ee9e2e999b3569b5a95d0ee733d41e59
Andres Arango <andresarangov17@gmail.com>
2017-05-11 21:09:21 -0400

    add show, new, edit, create, update, destroy methods to Animals Controller

--- animalfarm_rails_api/app/controllers/animals_controller.rb
+++ animalfarm_rails_api/app/controllers/animals_controller.rb
@@ -1,3 +1,44 @@
 class AnimalsController < ApplicationController
+  def show
+    @location = location.find(params[:location_id])
+    @animal = Animal.find(params[:id])
+  end
 
+  def new
+    @location = Location.find(params[:location_id])
+    @animal = Animal.new
+  end
+
+  def edit
+    @location = Location.find(params[:location_id])
+    @animal = Animal.find(params[:id])
+  end
+
+  def create
+    @location = Location.find(params[:location_id])
+    @animal = @location.animals.new(animal_params)
+    if @animal.save
+      redirect_to location_path(@location)
+    else
+      render :new
+  end
+
+  def update
+    @location = Location.find(params[:location_id])
+    @animal = @location.animals.find(params[:id])
+    @animal.update(animal_params)
+    redirect_to location_animal_path(@location, @animal)
+  end
+
+  def destroy
+    @location = Location.find(params[:location_id])
+    @animal = @location.animals.find(params[:id])
+    @animal.destroy
+    redirect_to location_path(@location)
+  end
+
+  private
+  def animal_params
+    params.require(:animal).permit(:animal_type, :name, :age, :is_adopted, :photo_url, :preferences, :days_left)
+  end
 end
